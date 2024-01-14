e67a43701551dcf053a73f5c4348c766a7451795
Cousar Nasiry Hanis <150012774+Cousar@users.noreply.github.com>
2024-01-14 10:57:27 +0100

    Add files via upload

---
+++ EX03_Q9.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+import numpy as np
+import requests
+from io import StringIO
+
+import matplotlib.pyplot as plt
+
+github_url = 'https://raw.githubusercontent.com/xaratustrah/teach/main/appacc2324/2016-07-11_ipm_data.txt'
+
+response = requests.get(github_url)
+
+data = np.genfromtxt(StringIO(response.text), dtype=float)
+
+x = np.arange(len(data)).reshape(len(data), 1)
+
+max_value = np.max(data)
+max_index = np.argmax(data)
+
+
+fig, ax = plt.subplots(figsize=(14, 10))
+fig.patch.set_facecolor('black')  
+
+# data with a specific line color
+ax.plot(x, data, color='blue', label='Data') 
+
+#axis background color to black
+ax.set_facecolor('black') 
+
+# Mark  the maximum with a red dot
+ax.scatter(max_index, max_value, color='red', label='Max Value')
+
+
+ax.set_xlabel('Index', color='white')
+ax.set_ylabel('Value', color='white')
+ax.set_title('Matrix Plot with Max Value Highlighted', color='white')
+
+
+ax.tick_params(axis='both', colors='white')
+ax.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
